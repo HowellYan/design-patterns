7e0b940b0005d2ef39b16d25f7052b7f06ebbda1
HowellYang <1726474652@qq.com>
2019-05-25 12:47:19 +0800

    templateMethod

---
+++ src/main/java/com/atomscat/templateMethod/ComputerScience.java
@@ -0,0 +1,10 @@
+package com.atomscat.templateMethod;
+
+/**
+ * 计算机学科类
+ */
+public class ComputerScience extends BasicEngineering  {
+    public void SpecialPaper() {
+        System.out.println("Object Oriented Programming");
+    }
+}
---
+++ src/main/java/com/atomscat/templateMethod/Electronics.java
@@ -0,0 +1,10 @@
+package com.atomscat.templateMethod;
+
+/**
+ * 电力电子技术
+ */
+public class Electronics extends BasicEngineering {
+    public void SpecialPaper() {
+        System.out.println("Digital Logic and Circuit Theory");
+    }
+}
